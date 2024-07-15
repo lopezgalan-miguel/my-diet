f26175cba40bf8a980739c4f596739a05c834c36
lopezgalan-miguel <57156297+lopezgalan-miguel@users.noreply.github.com>
2024-07-16 00:55:20 +0200

    feat: styling login

--- app/src/router/AppRouter.tsx
+++ app/src/router/AppRouter.tsx
@@ -1,12 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from '../pages/login';
+import Main from '../pages/main';
 
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
+            <Route path="*" element={<Navigate to="login" />} />
             <Route path="login" element={<Login />} />
+            <Route path="main" element={<Main />} />
         </Routes>
     </>
   )
